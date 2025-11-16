22cae2e1bc41aa1744604cfd5555e60de974d81e
camillefagbedji69-cpu <camillefagbedji69@gmail.com>
2025-11-16 19:27:19 +0100

    Add files via upload

---
+++ App_reconnaissance faciale.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+import numpy as np 
+from PIL import Image
+import pickle 
+
+##Affichage du dashboard 
+
+st.title('Démo de reconnaissance faciale')
+
+##chargement du modèle
+
+model = pickle.load(open("model.pkl", "rb"))
+
+## Chargement du fichier
+
+uploaded_file = st.file_uploader("Télécharger une image", type = ['jpeg', 'jpg', 'png'])
+
+if uploaded_file is not None : 
+    image = Image.open(uploaded_file)
+    st.image(image, caption = "Image téléchargée", use_column_width= True)
+
+
+    st.write("Traitement en cours....")
+
+    
+    # prétraitement en vecteur compatible KNN
+    img_array = np.array(image.resize((160, 160)))
+    img_vector = img_array.flatten().reshape(1, -1)
+
+    with st.spinner('Prédiction en cours...'):
+        prediction = model.predict(img_vector)
+    
+st.success(f"La personne reconnue est : {prediction[0]}")
